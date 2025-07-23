8461bcc790f2fb9db99b15ef9d749fbc4562f260
Behnam Mozaffari <your-actual-email@example.com>
2025-07-23 13:23:10 +0200

    Auto save: urls.py

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,10 +1,9 @@
-"""URL patterns."""
+# URL patterns
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.user_notifications, name="user-notifications"),
-    path("<int:pk>/read/", views.mark_notification_read, name="mark-notification-read"),
-    path("contact/", views.contact_us, name="contact-us"),
+    path('', views.user_notifications, name='user_notifications'),
+    path('<int:pk>/read/', views.mark_notification_read, name='mark_notification_read'),
+    path('contact/', views.contact_us, name='contact_us'),
 ]
